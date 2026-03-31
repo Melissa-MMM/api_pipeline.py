3a4c008939507311d1fa6d5c962d5bb87868caeb
Melissa Muluvhahothe <119665551+Melissa-MMM@users.noreply.github.com>
2026-03-31 17:53:43 +0200

    Update api_pipeline.py

--- api_pipeline.py
+++ api_pipeline.py
@@ -3,26 +3,24 @@ import pandas as pd
 import sqlite3
 from datetime import datetime
 
-# STEP 1: Extract (API request)
-url = "https://api.exchangerate.host/latest"
+# STEP 1: Extract
+url = "https://api.exchangerate.host/latest?base=EUR"
 response = requests.get(url)
 data = response.json()
 
-# STEP 2: Transform
-base = data['base']
-date = data['date']
-rates = data['rates']
+print(data)  
 
-# Extract a few currencies
-usd = rates['USD']
-eur = rates['EUR']
-gbp = rates['GBP']
-zar = rates['ZAR']
+# STEP 2: Transform (safe access)
+rates = data.get('rates', {})
 
+usd = rates.get('USD')
+eur = rates.get('EUR')
+gbp = rates.get('GBP')
+zar = rates.get('ZAR')
+ 
 # Create DataFrame
 df = pd.DataFrame([{
-    "base_currency": base,
-    "date": date,
+    "base_currency": "EUR",
     "usd_rate": usd,
     "eur_rate": eur,
     "gbp_rate": gbp,
@@ -30,7 +28,7 @@ df = pd.DataFrame([{
     "timestamp": datetime.now()
 }])
 
-# STEP 3: Load (SQLite)
+# STEP 3: Load
 conn = sqlite3.connect("api_pipeline.db")
 
 df.to_sql("exchange_rates", conn, if_exists="append", index=False)
